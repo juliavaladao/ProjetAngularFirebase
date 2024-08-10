a0cf326d162fd964724b26e23862484f9b0ea5c7
Eduardo Pellegrino <162513336+eduardoCope@users.noreply.github.com>
2024-08-10 00:29:32 +0000

    Montagem Curriculo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,30 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  pessoa = {
+    nome: 'Papai Smurf',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7zpM06RygbeSXHnrVk3cQTPVBushprWqUcc3S0IDHTfPDKzO_J20_MGH1LpeTutmlKhE&usqp=CAU',
+    objetivo: 'Doutorado em Quimica',
+    contato: {
+      email:'daddy@gmail.com',
+      telefone:'(21)96904-4469',
+      linkedin: 'linkedin.com/smurfPapai'
+    },
+    softskill: ['Dominío de poções', 'Smurfês: fluente', 'Mestre do onlyfans', 'Atendimento ao cliente'],
+    formacao: [
+      {
+        ano_inicio:'300 a.C',
+        ano_termino:'40',
+        instituicao:'Vila Mágica',
+        curso:'Poções e Magia natural'
+      },
+      {
+        ano_inicio:'2020',
+        ano_termino:'2023',
+        instituicao:'Universidade Federal Pipokinha',
+        curso:'Majistério em putaria'
+      },
+    ]
+  }
   constructor(){ }
 }
