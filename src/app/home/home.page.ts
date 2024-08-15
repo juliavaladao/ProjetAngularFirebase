cff4138c33312603a81e1591a7ed37526fbc27b6
juliavaladao <162513162+juliavaladao@users.noreply.github.com>
2024-08-15 18:54:07 +0000

    perfil_post2
    perfil_post2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,30 +10,40 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  pessoa = {
-    nome: 'Papai Smurf',
-    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7zpM06RygbeSXHnrVk3cQTPVBushprWqUcc3S0IDHTfPDKzO_J20_MGH1LpeTutmlKhE&usqp=CAU',
-    objetivo: 'Doutorado em Quimica',
-    contato: {
-      email:'daddy@gmail.com',
-      telefone:'(21)96904-4469',
-      linkedin: 'linkedin.com/smurfPapai'
+  perfil = {
+    foto: 'https://wallpapers.com/images/featured/imagens-de-perfil-do-discord-p2o9voc08wbr2cs9.jpg',
+    nome: 'Julia Valadão',
+    profissao: 'T.I.',
+    user: '@juliavaladao',
+    idioma: 'Portugues',
+    cidade: 'Tatuí/SP',
+    desde: 'Agosto 2020',
+    bio: '🐱‍👤',
+    metricas: {
+      curtidas: '12k',
+      seguidores: '1k',
+      amigos: '800'
     },
-    softskill: ['Dominío de poções', 'Smurfês: fluente', 'Mestre do onlyfans', 'Atendimento ao cliente'],
-    formacao: [
+    postAmgs: [
       {
-        ano_inicio:'300 a.C',
-        ano_termino:'40',
-        instituicao:'Vila Mágica',
-        curso:'Poções e Magia natural'
+        img: 'https://i.pinimg.com/236x/71/f2/13/71f213e01bca74899d502c6287a40057.jpg',
+        nome: 'Pedro Rocha',
+        user: '@oldrocha',
+        post: 'amo minha mulher'
       },
       {
-        ano_inicio:'2020',
-        ano_termino:'2023',
-        instituicao:'Universidade Federal Pipokinha',
-        curso:'Majistério em putaria'
+        img: 'https://i.pinimg.com/736x/14/10/99/1410991d71fc25089950a8dcfd412d48.jpg',
+        nome: 'Malu (Taylor`s Version)',
+        user: '@luizaantuness',
+        post: 'Odeio a Beyonce, Doja Cat, The Weeknd e Rihanna mas n sei dizer o pq'
       },
+      {
+        img: 'https://imagens.net.br/wp-content/uploads/2023/08/tristes-imagens-de-perfil-do-whatsapp-para-expressar-sua-tristeza-19.jpg',
+        nome: 'Eduardo Pellegrino',
+        user: '@dududududuuuu2',
+        post: 'ai gente quue frio'
+      }
     ]
   }
-  constructor(){ }
+
 }
