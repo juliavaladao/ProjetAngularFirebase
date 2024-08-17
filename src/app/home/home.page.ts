de80b903b7403d7ed6494d9565f2b9b063bca7a0
juliavaladao <162513162+juliavaladao@users.noreply.github.com>
2024-08-17 00:30:18 +0000

    perfil_twt

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,12 +13,12 @@ export class HomePage {
   perfil = {
     foto: 'https://wallpapers.com/images/featured/imagens-de-perfil-do-discord-p2o9voc08wbr2cs9.jpg',
     nome: 'Julia Valadão',
-    profissao: 'T.I.',
+    profissao: 'Programadora',
     user: '@juliavaladao',
-    idioma: 'Portugues',
+    idioma: 'Português',
     cidade: 'Tatuí/SP',
-    desde: 'Agosto 2020',
-    bio: '🐱‍👤',
+    desde: 'desde outubro 2019',
+    bio: '👾',
     metricas: {
       curtidas: '12k',
       seguidores: '1k',
@@ -27,21 +27,21 @@ export class HomePage {
     postAmgs: [
       {
         img: 'https://i.pinimg.com/236x/71/f2/13/71f213e01bca74899d502c6287a40057.jpg',
-        nome: 'Pedro Rocha',
-        user: '@oldrocha',
+        nome: 'Rocha🍂',
+        user: '@sir__rochinha',
         post: 'amo minha mulher'
       },
       {
         img: 'https://i.pinimg.com/736x/14/10/99/1410991d71fc25089950a8dcfd412d48.jpg',
         nome: 'Malu (Taylor`s Version)',
-        user: '@luizaantuness',
-        post: 'Odeio a Beyonce, Doja Cat, The Weeknd e Rihanna mas n sei dizer o pq'
+        user: '@the1lovergirl',
+        post: 'odeio todos'
       },
       {
         img: 'https://imagens.net.br/wp-content/uploads/2023/08/tristes-imagens-de-perfil-do-whatsapp-para-expressar-sua-tristeza-19.jpg',
-        nome: 'Eduardo Pellegrino',
-        user: '@dududududuuuu2',
-        post: 'ai gente quue frio'
+        nome: 'du²',
+        user: '@edududuuduuu',
+        post: 'EU VOU MORRER DE HIPOTERMIAAAAAA'
       }
     ]
   }
